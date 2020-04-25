3b68e363f5af1cabd24a3edee5388f0a75c6ae3c
HeLinChooi <helinchooi@gmail.com>
2020-04-25 18:21:13 +0800

    add one block button added

--- onePageScript.js
+++ onePageScript.js
@@ -11,3 +11,15 @@
 function toLanding(){
   window.location.href = "landing.html";
 }
+function addOne(){
+  console.log("in function");
+  document.getElementById("oneExtraGrid").innerHTML = `
+  <div class="one_grid">
+    <div>Bachelor of East Asian Studies</div>
+    <div>UM</div>
+    <span><a href="www.google.com">Uni Malaya</a></span>
+    <span>KL</span>
+    <span>Rank : <span>70</span></span>
+    <span>Minimum CGPA : <span>2.5</span></span>
+  </div>`
+}
