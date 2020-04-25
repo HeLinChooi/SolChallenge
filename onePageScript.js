7fe25864e05c3fa68a8abe2eb8380d5271f3a42b
HeLinChooi <helinchooi@gmail.com>
2020-04-25 15:42:39 +0800

    2 pages site

--- onePageScript.js
+++ onePageScript.js
@@ -1,15 +1,13 @@
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-  // var database = firebase.database();
-  // document.getElementById("mySidePanel").style.width = "50%";
-  console.log("in open nav function");
-  // firebase.database().ref("user").set({
-  //   username: "steady"
-  // });
-  document.getElementById("mySidePanel").style.width = "90%";
-}
+// /* Set the width of the sidebar to 250px (show it) */
+// function openNav() {
+//   console.log("in open nav function");
+//   document.getElementById("mySidePanel").style.width = "90%";
+// }
 
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById("mySidePanel").style.width = "0";
+// /* Set the width of the sidebar to 0 (hide it) */
+// function closeNav() {
+//   document.getElementById("mySidePanel").style.width = "0";
+// }
+function toLanding(){
+  window.location.href = "landing.html";
 }
