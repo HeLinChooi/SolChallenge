a23cc933d6b78a569ddf19a03d33f77b39cf95b7
HeLinChooi <helinchooi@gmail.com>
2020-04-26 14:24:58 +0800

    final ver

--- onePageScript.js
+++ onePageScript.js
@@ -1,13 +1,3 @@
-// /* Set the width of the sidebar to 250px (show it) */
-// function openNav() {
-//   console.log("in open nav function");
-//   document.getElementById("mySidePanel").style.width = "90%";
-// }
-
-// /* Set the width of the sidebar to 0 (hide it) */
-// function closeNav() {
-//   document.getElementById("mySidePanel").style.width = "0";
-// }
 function toLanding(){
   window.location.href = "landing.html";
 }
