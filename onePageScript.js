48a06c1d28cd41b4c0fbb0b827f32d24fc125e73
HeLinChooi <helinchooi@gmail.com>
2020-04-26 14:26:39 +0800

    Revert "final ver"
    This reverts commit a23cc933d6b78a569ddf19a03d33f77b39cf95b7.

--- onePageScript.js
+++ onePageScript.js
@@ -1,3 +1,13 @@
+// /* Set the width of the sidebar to 250px (show it) */
+// function openNav() {
+//   console.log("in open nav function");
+//   document.getElementById("mySidePanel").style.width = "90%";
+// }
+
+// /* Set the width of the sidebar to 0 (hide it) */
+// function closeNav() {
+//   document.getElementById("mySidePanel").style.width = "0";
+// }
 function toLanding(){
   window.location.href = "landing.html";
 }
